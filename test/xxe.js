bd3ac78231acc3810d73c2b849661d968c0f2894
Shubham Gangurde <shubhamgangurde16@gmail.com>
2023-11-27 19:13:58 +0530

    fix applied

---
+++ test/xxe.js
@@ -0,0 +1,25 @@
+import { expect } from 'chai';
+import request from 'supertest';
+import express from 'express';
+import bodyParser from 'body-parser';
+import libxmljs from 'libxmljs';
+
+const app = express();
+app.use(bodyParser.text({ type: '*/*' }));
+
+describe('XXE Attack Test', () => {
+  it('should prevent XXE attack', (done) => {
+    const maliciousXml = '<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE foo [<!ENTITY xxe SYSTEM "file:///etc/passwd">]><root>&xxe;</root>';
+
+    request(app)
+      .post('/xxe')
+      .send(maliciousXml)
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err);
+
+        expect(res.text).to.not.contain('/etc/passwd');
+        done();
+      });
+  });
+});
