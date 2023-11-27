cbfa0cb023b11030d4a8fe35c7bf08191989bedd
Yogendra Jaiswal <yogendra@incubyte.co>
2023-11-27 17:43:08 +0530

    fix applied

--- config.js
+++ config.js
@@ -10,10 +10,11 @@ const expressSession = require('express-session');
 app.use(helmet());
 app.use(bodyParser.json());
 app.use(expressSession({
-    secret: 'your-secret-key',
-    resave: false,
-    saveUninitialized: true,
-    cookie: { secure: true, httpOnly: true, expires: new Date(Date.now() + 3600000) }
+    name: 'my-session-cookie',
+    secret: 'my-secret-key',
+    cookie: {
+        httpOnly: true
+    }
 }));
 
 app.get('/example', function(req, res) {
--- test/config.js
+++ test/config.js
@@ -1,23 +1,12 @@
 import request from 'supertest';
-import express from 'express';
+import app from './app';
 
-const app = express();
+describe('Session Middleware', () => {
+  it('should set the httpOnly flag for session cookies', async () => {
+    const response = await request(app).get('/example');
+    const setCookieHeader = response.headers['set-cookie'];
 
-app.use(expressSession({
-    secret: 'mysecret',
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-        secure: true,
-        httpOnly: true,
-        sameSite: 'strict',
-        expires: new Date(Date.now() + 3600000) // Set expiration date for persistent cookies
-    }
-}));
-
-describe('Security Test', () => {
-    it('should return "I\'m in danger!"', async () => {
-        const response = await request(app).get('/example');
-        expect(response.text).toBe("I'm in danger!");
-    });
+    expect(setCookieHeader).toBeDefined();
+    expect(setCookieHeader).toContain('HttpOnly');
+  });
 });
