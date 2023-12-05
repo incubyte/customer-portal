81de12738ed01611fa76db1e7650cdc551479eb4
Shubham Gangurde <shubhamgangurde16@gmail.com>
2023-12-05 10:30:43 +0530

    fix: resolves issue A01:2021 - Broken Access Control

--- config.js
+++ config.js
@@ -6,6 +6,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
 const expressSession = require('express-session');
+const csurf = require('csurf');
 
 app.use(helmet());
 app.use(bodyParser.json());
@@ -17,6 +18,7 @@ app.use(expressSession({
         domain: 'example.com' // Set the domain of the cookie to match the server's domain
     }
 }));
+app.use(csurf());
 
 app.get('/example', function(req, res) {
     res.end(`I'm in danger!`);
--- test/config.js
+++ test/config.js
@@ -1,28 +1,10 @@
-Here is the test code for the part of the code where the security issue is present:
+import request from 'supertest';
+import app from './app';
 
-```javascript
-const request = require('supertest');
-const app = require('./app');
-
-describe('GET /example', () => {
-  it('should return a 200 status code', async () => {
-    const res = await request(app).get('/example');
-    expect(res.statusCode).toBe(200);
-  });
-
-  it('should not expose sensitive information', async () => {
-    const res = await request(app).get('/example');
-    expect(res.text).not.toContain("I'm in danger!");
+describe('CSRF Middleware', () => {
+  it('should include CSRF middleware', async () => {
+    const response = await request(app).get('/example');
+    expect(response.status).toBe(403);
+    expect(response.text).toBe('Forbidden');
   });
 });
-```
-
-Please note that this test assumes that you have set up the Express app in a separate file named `app.js`. You may need to modify the import statement `const app = require('./app');` to match the actual file path.
-
-Make sure to install the `supertest` package before running the test:
-
-```
-npm install supertest --save-dev
-```
-
-This test will check if the `/example` endpoint returns a 200 status code and does not expose the sensitive information "I'm in danger!".
