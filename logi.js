0e39bd5ec81d1ad8e4e32e1ef7baed17b9d53561
Yogendra Jaiswal <yogendra@incubyte.co>
2024-01-18 21:30:35 +0530

    fix: resolves issue A01:2021 - Broken Access Control

--- logi.js
+++ logi.js
@@ -2,7 +2,31 @@
 
 const port = 3000;
 const express = require('express');
+'use strict';
+
+const port = 3000;
+const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const urlencoded = bodyParser.urlencoded;
+const staticServer = express.static;
+const csrf = require('csurf');
+
+app.use(urlencoded({ extended: true }));
+app.use('/', staticServer('./static/'));
+app.use(csrf());
+
+app.post('/login', function(req, res) {
+    // We trust our users, every login will be successful!
+    const username = req.body.username;
+    const password = req.body.password;
+
+    // Enterprise-grade logging FTW!
+    console.log(`${username} logged in with the password: ${password}.`);
+    res.end('Logged in with the honor system');
+});
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
 const bodyParser = require('body-parser');
 const urlencoded = bodyParser.urlencoded;
 const staticServer = express.static;
